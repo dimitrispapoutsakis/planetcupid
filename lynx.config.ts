f12d4fe9ccfc2bee189ce720dd5fa3aa38945e0e
Dimitris <dimitrispapoutakis@gmail.com>
2025-10-23 17:40:08 +0300

    feat: React router.

--- lynx.config.ts
+++ lynx.config.ts
@@ -4,8 +4,6 @@ import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin';
 import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 import { pluginSass } from '@rsbuild/plugin-sass';
-import { tanstackRouter } from '@tanstack/router-plugin/rspack'
-
 
 export default defineConfig({
   plugins: [
@@ -19,16 +17,6 @@ export default defineConfig({
     pluginTypeCheck(),
     pluginSass(),
   ],
-  tools: {
-    rspack: {
-      plugins: [
-        tanstackRouter({
-          target: 'react',
-          autoCodeSplitting: true,
-        }),
-      ],
-    },
-  },
   resolve: {
     alias: {
       '@': './src',
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import '@lynx-js/preact-devtools';
 import '@lynx-js/react/debug';
 import { root } from '@lynx-js/react';
 import './assets/scss/index.scss';
+import { MemoryRouter, Routes, Route } from 'react-router';
 
 import { App } from '@/components/App/App';
 
-root.render(<App />);
+root.render(
+  <MemoryRouter initialEntries={['/']}>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </MemoryRouter>
+);
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
